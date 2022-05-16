627a24ad70d81f6df077f80c9fcb2fb7b9278a85
xanedesu <xanedesu@gmail.com>
2022-05-16 14:24:33 +0300

    Обновил подключение к websocket

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,9 +3,48 @@ import { setContext } from '@apollo/client/link/context';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { createUploadLink } from 'apollo-upload-client';
-import { createClient } from 'graphql-ws';
+import { Client, ClientOptions, createClient } from 'graphql-ws';
 
 const BACKEND_URI = 'http://192.168.0.2:8020/graphql';
+
+interface RestartableClient extends Client {
+  restart(): void;
+}
+
+const createRestartableClient = (options: ClientOptions): RestartableClient => {
+  let restartRequested = false;
+  let restart = () => {
+    restartRequested = true;
+  };
+
+  const client = createClient({
+    ...options,
+    on: {
+      ...options.on,
+      opened: (socket: any) => {
+        options.on?.opened?.(socket);
+
+        restart = () => {
+          if (socket.readyState === WebSocket.OPEN) {
+            socket.close(4205, 'Client Restart');
+          } else {
+            restartRequested = true;
+          }
+        };
+
+        if (restartRequested) {
+          restartRequested = false;
+          restart();
+        }
+      },
+    },
+  });
+
+  return {
+    ...client,
+    restart: () => restart(),
+  };
+};
 
 const getAuthorization = () => {
   return 'Bearer '.concat(localStorage.getItem('token'));
@@ -15,13 +54,12 @@ const wsLink =
   typeof window === 'undefined'
     ? null
     : new GraphQLWsLink(
-        createClient({
+        createRestartableClient({
           url: BACKEND_URI.replace('http', 'ws'),
           connectionParams: () => ({
             authorization: getAuthorization(),
           }),
           shouldRetry: () => true,
-          retryAttempts: 5,
         }),
       );
 
