06ea3e16043045531be50695ffd25c21f8d0df66
xanedesu <xanedesu@gmail.com>
2022-05-19 23:55:05 +0300

    фикс

--- src/components/chats/layout/component.tsx
+++ src/components/chats/layout/component.tsx
@@ -14,7 +14,13 @@ export const ChatsLayout: React.FC<ChatsLayoutProps> = ({ children }) => {
   return (
     <Layout i18n='common:pages.chats'>
       <Box display='flex' width='100%' height='100%' bgcolor='#f7f7f7'>
-        <Box display='flex' flexDirection='column' width='100%' height='100%' maxWidth={360}>
+        <Box
+          display='flex'
+          flexDirection='column'
+          flexShrink={0}
+          width='100%'
+          height='100%'
+          maxWidth={360}>
           <ChatList />
         </Box>
         <Box display='flex' flexDirection='column' flexGrow={1} borderLeft='1px solid #0002'>
