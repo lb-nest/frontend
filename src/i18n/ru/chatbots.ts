881d2261ebe177616e69654a3e1b13e00765b6fb
xanedesu <xanedesu@gmail.com>
2022-06-15 00:38:42 +0300

    i18n конструктора чатботов

--- src/i18n/ru/chatbots.ts
+++ src/i18n/ru/chatbots.ts
@@ -1,4 +1,5 @@
 import { ResourceLanguage } from 'i18next';
+import { NodeType } from '../../components/chatbots/editor/types';
 
 export const chatbots: ResourceLanguage = {
   create: 'Создать',
@@ -21,5 +22,35 @@ export const chatbots: ResourceLanguage = {
     submit: '',
     cancel: '',
   },
-  editor: {},
+  editor: {
+    nodes: {
+      [NodeType.Start]: {
+        title: 'Начало',
+      },
+      [NodeType.SendMessage]: {
+        title: 'Отправка сообщения',
+      },
+      [NodeType.CollectInput]: {
+        title: 'Сбор входных данных',
+      },
+      [NodeType.Buttons]: {
+        title: 'Кнопки',
+      },
+      [NodeType.Branch]: {
+        title: 'Ветвление',
+      },
+      [NodeType.ServiceCall]: {
+        title: 'Вызов стороннего API',
+      },
+      [NodeType.Transfer]: {
+        title: 'Назначение',
+      },
+      [NodeType.AssignTag]: {
+        title: 'Присвоение тега',
+      },
+      [NodeType.Close]: {
+        title: 'Закрытие обращения',
+      },
+    },
+  },
 };
